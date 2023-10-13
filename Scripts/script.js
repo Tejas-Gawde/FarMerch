63dfd2283b7ef6af9b1b7ff58a71894f7671fd54
Tejas <tejasg263@gmail.com>
2023-10-13 10:42:22 +0530

    Last minute changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,8 +3,7 @@ import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
 "https://xkbaxhajuykyvvzstdfn.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrYmF4aGFqdXlreXZ2enN0ZGZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ3MDEwNTEsImV4cCI6MjAxMDI3NzA1MX0.ymfXSwBn9oK3-M3e597xHgR-m16H7yyAf1mbmJrbEmw"
-);
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrYmF4aGFqdXlreXZ2enN0ZGZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ3MDEwNTEsImV4cCI6MjAxMDI3NzA1MX0.ymfXSwBn9oK3-M3e597xHgR-m16H7yyAf1mbmJrbEmw");
 
 // References
 var regEmail = document.getElementById("regEmail");
@@ -48,12 +47,12 @@ async function loginUser(){
         alert("Login Successfull");
         console.log(data);
         loggedIn=true;
-        logincheck(data.user.email);
+        logincheck(data);
     }
 }
 
 // FUnction to check login 
-async function logincheck(data.user.email){
+async function logincheck(data){
     if(loggedIn){
         status.innerText=`User status : Logged in by email - ${data.user.email}`;
     }
