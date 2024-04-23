ddb40570b8c7e294caf7332863262404033fe1cf
AtharvGholap04 <atharvgholap2004@gmail.com>
2024-04-23 15:48:44 +0530

    Minor changes

--- public/javascripts/supabase-backend-functions.js
+++ public/javascripts/supabase-backend-functions.js
@@ -2,64 +2,70 @@ import { sortCart } from "./backend-functions.js";
 import { supabase } from "./supabase.js";
 
 export async function fetchDataLimit() {
-    let { data: items, error } = await supabase.from('items')
-        .select('*')
-        .limit(6)
+    let { data: items, error } = await supabase
+        .from("items")
+        .select("*")
+        .limit(6);
     if (error) {
-        console.log(error)
-    }
-    else {
+        console.log(error);
+    } else {
         console.log("success");
-        return items
+        return items;
     }
 }
 
 export async function fetchData() {
-    let { data: items, error } = await supabase.from('items')
-        .select('*')
+    let { data: items, error } = await supabase.from("items").select("*");
     if (error) {
-        console.log(error)
-    }
-    else {
+        console.log(error);
+    } else {
         console.log("success");
-        return items
+        return items;
     }
 }
 
 export async function fetchSingleData(itemName) {
-    let { data: items, error } = await supabase.from('items')
+    let { data: items, error } = await supabase
+        .from("items")
         .select()
-        .eq('name', itemName)
-        .single()
+        .eq("name", itemName)
+        .single();
     if (error) {
-        console.log(error)
-    }
-    else {
+        console.log(error);
+    } else {
         console.log("got single data");
         return items;
     }
 }
 
 export async function fetchCategory(categoryName) {
-    let { data: items, error } = await supabase.from('items')
+    let { data: items, error } = await supabase
+        .from("items")
         .select()
-        .eq('category', categoryName)
+        .eq("category", categoryName);
     if (error) {
-        console.log(error)
-    }
-    else {
+        console.log(error);
+    } else {
         console.log("got category data");
-        return items
+        return items;
     }
 }
 
 export async function getSession() {
-    const { data, error } = await supabase.auth.getSession()
+    const { data, error } = await supabase.auth.getSession();
     if (data.session != null) {
-        console.log('Session:', data.session.user.id);
+        console.log("Session:", data.session.user.id);
         return data.session.user.id;
-    }
-    else console.log('No session found');
+    } else console.log("No session found");
+}
+
+export async function getuserType() {
+    const { data, error } = await supabase.auth.getSession();
+    if (data.session != null) {
+        console.log("Session:", data.session.user.id);
+        console.log(data.session.user.user_metadata);
+        return data.session.user.user_metadata.type;
+    } else console.log("No session found");
 }
 
 export async function signUpNewUser(email, password, username) {
@@ -77,7 +83,7 @@ export async function signUpNewUser(email, password, username) {
         return error.message;
     } else {
         const userId = await getSession();
-        console.log(data)
+        console.log(data);
         return await createUserRow(userId);
     }
 }
@@ -96,10 +102,10 @@ async function createUserRow(value) {
 
 export async function fetcharray(UserID) {
     let { data: users, error } = await supabase
-        .from('users')
-        .select('cart')
-        .eq('userID', UserID)
-        .single()
+        .from("users")
+        .select("cart")
+        .eq("userID", UserID)
+        .single();
     console.log("Fetching cart array for user: ", users);
     return users;
 }
@@ -107,32 +113,30 @@ export async function fetcharray(UserID) {
 export async function addToCart(cartData) {
     const UserID = await getSession();
     if (UserID == null) {
-        return 'No user logged in'
+        return "No user logged in";
     }
     const fetchedArray = await fetcharray(UserID);
-    fetchedArray.cart.push(cartData)
+    fetchedArray.cart.push(cartData);
     const cart = sortCart(fetchedArray.cart);
-    console.log('sorted cart is', cart);
+    console.log("sorted cart is", cart);
     const { data, error } = await supabase
-        .from('users')
+        .from("users")
         .update({ cart: cart })
-        .eq('userID', UserID)
-        .select()
+        .eq("userID", UserID)
+        .select();
     if (error) {
-        console.log(error)
-    }
-    else return 'Item added to cart!'
+        console.log(error);
+    } else return "Item added to cart!";
 }
 
 export async function loginUser(email, password) {
     const { data, error } = await supabase.auth.signInWithPassword({
         email: email,
         password: password,
-    })
+    });
     if (error) {
         return error.message;
-    }
-    else {
+    } else {
         getSession();
         return "User logged in successfully";
     }
@@ -140,21 +144,21 @@ export async function loginUser(email, password) {
 
 export async function fetchCartDetails(id) {
     const { data, error } = await supabase
-        .from('items')
-        .select('name, price, availableIn, url, id')
-        .in('id', id)
-    if (error) console.log(error)
+        .from("items")
+        .select("name, price, availableIn, url, id")
+        .in("id", id);
+    if (error) console.log(error);
     else return data;
 }
 
 export async function fetchID() {
     const UserID = await getSession();
     const { data, error } = await supabase
-        .from('users')
-        .select('cartID')
-        .eq('userID', UserID)
-        .single()
-    if (error) console.log(error)
+        .from("users")
+        .select("cartID")
+        .eq("userID", UserID)
+        .single();
+    if (error) console.log(error);
     else {
         return data.cartID;
     }
@@ -163,11 +167,11 @@ export async function fetchID() {
 export async function fetchCart() {
     const UserID = await getSession();
     const { data, error } = await supabase
-        .from('users')
-        .select('cart')
-        .eq('userID', UserID)
-        .single()
-    if (error) console.log(error)
+        .from("users")
+        .select("cart")
+        .eq("userID", UserID)
+        .single();
+    if (error) console.log(error);
     else {
         return data.cart;
     }
@@ -176,14 +180,14 @@ export async function fetchCart() {
 export async function updateCart(cart) {
     const UserID = await getSession();
     const { data, error } = await supabase
-        .from('users')
+        .from("users")
         .update({ cart: cart })
-        .eq('userID', UserID)
-        .select()
+        .eq("userID", UserID)
+        .select();
     if (error) {
-        console.log(error)
+        console.log(error);
     }
-    return 'Cart Updated'
+    return "Cart Updated";
 }
 
 export async function signUpNewSeller(email, password, username) {
@@ -201,8 +205,11 @@ export async function signUpNewSeller(email, password, username) {
         return error.message;
     } else {
         const userId = await getSession();
-        console.log(data)
-        return await createSellerRow(userId, data.user.user_metadata.display_name);
+        console.log(data);
+        return await createSellerRow(
+            userId,
+            data.user.user_metadata.display_name
+        );
     }
 }
 
@@ -220,9 +227,9 @@ async function createSellerRow(userID, userName) {
 
 async function signOutUser() {
     const { error } = await supabase.auth.signOut();
-    if (error) alert('Error signing out:', error.message);
+    if (error) alert("Error signing out:", error.message);
     else {
-        alert('Successfully signed out!');
+        alert("Successfully signed out!");
         getSession();
     }
 }
@@ -231,14 +238,13 @@ export async function loginSeller(email, password) {
     const { data, error } = await supabase.auth.signInWithPassword({
         email: email,
         password: password,
-    })
+    });
     if (error) {
         return error.message;
-    }
-    else {
+    } else {
         if (data.user.user_metadata.type === "Seller") {
             return "Seller logged in successfully";
-            console.log(data)
+            console.log(data);
         } else {
             signOutUser();
             return "Not A seller account";
@@ -247,4 +253,3 @@ export async function loginSeller(email, password) {
     getSession();
 }
 ///// BE SURE TO REPLACE fetchCart and fetchArray..... (Pending)
-
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 // Imports
-import express from 'express';
-import dotenv from 'dotenv';
-import stripe from 'stripe';
-import { fetchData, fetchDataLimit, fetchSingleData, fetchCategory, signUpNewUser, addToCart, fetchID, fetchCartDetails, loginUser, fetchCart, updateCart, loginSeller, signUpNewSeller } from '../public/javascripts/supabase-backend-functions.js';
-import { combineCartAndProductData, removeFromCart, updateCartQuantity } from '../public/javascripts/backend-functions.js';
+import express from "express";
+import dotenv from "dotenv";
+import stripe from "stripe";
+import {
+    fetchData,
+    fetchDataLimit,
+    fetchSingleData,
+    fetchCategory,
+    signUpNewUser,
+    addToCart,
+    fetchCartDetails,
+    loginUser,
+    fetchCart,
+    updateCart,
+    loginSeller,
+    signUpNewSeller,
+    getuserType,
+} from "../public/javascripts/supabase-backend-functions.js";
+import {
+    combineCartAndProductData,
+    removeFromCart,
+    updateCartQuantity,
+} from "../public/javascripts/backend-functions.js";
 
 dotenv.config();
 // Declaration
@@ -14,164 +32,172 @@ const stripeGateway = stripe(process.env.STRIPE_API_KEY);
 const DOMAIN = process.env.DOMAIN;
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
-  const itemArrayLimit = await fetchDataLimit();
-  res.render('index', { itemArrayLimit });
+router.get("/", async (req, res, next) => {
+    const itemArrayLimit = await fetchDataLimit();
+    res.render("index", { itemArrayLimit });
 });
 
-router.get('/products', async (req, res) => {
-  const itemArray = await fetchData();
-  res.render("products", { itemArray });
+router.get("/products", async (req, res) => {
+    const itemArray = await fetchData();
+    res.render("products", { itemArray });
 });
 
-router.get('/products/:item', async (req, res) => {
-  const itemName = req.params.item;
-  const singleItem = await fetchSingleData(itemName);
-  res.render("productpage", { singleItem });
-})
-
-router.get('/category=:categoryName', async (req, res) => {
-  const itemName = req.params.categoryName;
-  const categoryData = await fetchCategory(itemName);
-  res.render("category", { categoryData, itemName });
-})
-
-router.post('/products', (req, res) => {
-  const { itemName, itemPrice } = req.body;
-  cartdata.push({ itemName: itemName, itemPrice: itemPrice })
-  console.log(cartdata);
-})
-
-router.post('/addtocart', async (req, res) => {
-  const { cartData } = req.body;
-  console.log('cartdaata is', cartData);
-  const response = await addToCart(cartData);
-  res.send(response);
-})
-
-router.post('/login', async (req, res) => {
-  const { Email, Password } = req.body;
-  const message = await loginUser(Email, Password);
-  res.send(JSON.stringify(message));
-})
-
-router.post('/sellerLogin', async (req, res) => {
-  const { Email, Password } = req.body;
-  const message = await loginSeller(Email, Password, 'seller');
-  res.send(JSON.stringify(message));
-})
-
-router.post('/sellerRegister', async (req, res) => {
-  const { Email, Password, Username } = req.body;
-  const message = await signUpNewSeller(Email, Password, Username);
-  res.send(JSON.stringify(message));
-})
-
-router.post('/register', async (req, res) => {
-  const { Email, Password, Username } = req.body;
-  const message = await signUpNewUser(Email, Password, Username);
-  res.send(JSON.stringify(message));
-})
-
-router.post('/update-cart', async (req, res) => {
-  const { id, quantity } = req.body;
-  const cart = await fetchCart();
-  const newCart = updateCartQuantity(cart, id, quantity)
-  await updateCart(newCart)
-  res.send("nice")
-})
-
-router.post('/remove-from-cart', async (req, res) => {
-  const { id } = req.body;
-  const cart = await fetchCart();
-  const newCart = removeFromCart(cart, id)
-  await updateCart(newCart)
-  res.send("nice")
-})
-
-router.get('/about-us', (req, res) => {
-  res.render("aboutus");
+router.get("/products/:item", async (req, res) => {
+    const itemName = req.params.item;
+    const singleItem = await fetchSingleData(itemName);
+    res.render("productpage", { singleItem });
 });
 
-router.get('/contact-us', (req, res) => {
-  res.render("contact");
+router.get("/category=:categoryName", async (req, res) => {
+    const itemName = req.params.categoryName;
+    const categoryData = await fetchCategory(itemName);
+    res.render("category", { categoryData, itemName });
 });
 
-router.get('/customer', (req, res) => {
-  res.render("customerpage");
+router.post("/products", (req, res) => {
+    const { itemName, itemPrice } = req.body;
+    cartdata.push({ itemName: itemName, itemPrice: itemPrice });
+    console.log(cartdata);
 });
 
-router.get('/seller', (req, res) => {
-  res.render("sellerpage");
+router.post("/addtocart", async (req, res) => {
+    const { cartData } = req.body;
+    console.log("cartdaata is", cartData);
+    const response = await addToCart(cartData);
+    res.send(response);
 });
 
-router.get('/upload', (req, res) => {
-  res.render("upload");
+router.post("/login", async (req, res) => {
+    const { Email, Password } = req.body;
+    const message = await loginUser(Email, Password);
+    res.send(JSON.stringify(message));
 });
 
-router.get('/vegetable', (req, res) => {
-  res.render("vegetable");
+router.post("/sellerLogin", async (req, res) => {
+    const { Email, Password } = req.body;
+    const message = await loginSeller(Email, Password, "seller");
+    res.send(JSON.stringify(message));
 });
 
-router.get('/fruits', (req, res) => {
-  res.render("fruits");
+router.post("/sellerRegister", async (req, res) => {
+    const { Email, Password, Username } = req.body;
+    const message = await signUpNewSeller(Email, Password, Username);
+    res.send(JSON.stringify(message));
 });
 
-router.get('/productpage', (req, res) => {
-  res.render("productpage");
+router.post("/register", async (req, res) => {
+    const { Email, Password, Username } = req.body;
+    const message = await signUpNewUser(Email, Password, Username);
+    res.send(JSON.stringify(message));
 });
 
-router.get('/cart', async (req, res) => {
-  const cart = await fetchCart();
-  if (cart) {
-    console.log(cart);
-    const cartItems = await fetchCartDetails(cart.map(item => item.id));
+router.post("/update-cart", async (req, res) => {
+    const { id, quantity } = req.body;
+    const cart = await fetchCart();
+    const newCart = updateCartQuantity(cart, id, quantity);
+    await updateCart(newCart);
+    res.send("nice");
+});
+
+router.post("/remove-from-cart", async (req, res) => {
+    const { id } = req.body;
+    const cart = await fetchCart();
+    const newCart = removeFromCart(cart, id);
+    await updateCart(newCart);
+    res.send("nice");
+});
+
+router.get("/stockhistory", async (req, res) => {
+    const state = await getuserType();
+    if (state == "Seller") {
+        res.render("stockhistory");
+    } else res.render("notseller");
+});
+
+router.get("/about-us", (req, res) => {
+    res.render("aboutus");
+});
+
+router.get("/contact-us", (req, res) => {
+    res.render("contact");
+});
+
+router.get("/customer", (req, res) => {
+    res.render("customerpage");
+});
+
+router.get("/seller", (req, res) => {
+    res.render("sellerpage");
+});
+
+router.get("/upload", (req, res) => {
+    res.render("upload");
+});
+
+router.get("/vegetable", (req, res) => {
+    res.render("vegetable");
+});
+
+router.get("/fruits", (req, res) => {
+    res.render("fruits");
+});
+
+router.get("/productpage", (req, res) => {
+    res.render("productpage");
+});
+
+router.get("/cart", async (req, res) => {
+    const cart = await fetchCart();
+    if (cart) {
+        console.log(cart);
+        const cartItems = await fetchCartDetails(cart.map((item) => item.id));
+        const finalCart = combineCartAndProductData(cart, cartItems);
+        res.render("cart", { finalCart });
+    } else {
+        res.render("cart", { finalCart: [] });
+    }
+});
+
+router.post("/stripe-checkout", async (req, res) => {
+    const cart = await fetchCart();
+    const cartItems = await fetchCartDetails(cart.map((item) => item.id));
     const finalCart = combineCartAndProductData(cart, cartItems);
-    res.render("cart", { finalCart });
-  }
-  else {
-    res.render("cart", { finalCart: [] });
-  }
-});
-
-router.post('/stripe-checkout', async (req, res) => {
-  const cart = await fetchCart();
-  const cartItems = await fetchCartDetails(cart.map(item => item.id));
-  const finalCart = combineCartAndProductData(cart, cartItems);
-  const finalProduct = finalCart.map(item => ({
-    price_data: {
-      currency: 'usd',
-      product_data: {
-        name: item.name,
-        images: [item.url]
-      },
-      unit_amount: Math.round(item.price * 100), // Convert price to cents and round
-    },
-    quantity: item.quantity,
-  }));
-  try {
-    const session = await stripeGateway.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: finalProduct,
-      mode: 'payment',
-      success_url: `${DOMAIN}/success`,
-      cancel_url: `${DOMAIN}/cancel`,
-    });
-    res.json({ sessionUrl: session.url });
-  } catch (error) {
-    console.error('Error creating Stripe Checkout session:', error);
-    res.status(500).json({ error: 'Failed to create Stripe Checkout session' });
-  }
+    const finalProduct = finalCart.map((item) => ({
+        price_data: {
+            currency: "usd",
+            product_data: {
+                name: item.name,
+                images: [item.url],
+            },
+            unit_amount: Math.round(item.price * 100), // Convert price to cents and round
+        },
+        quantity: item.quantity,
+    }));
+    try {
+        const session = await stripeGateway.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: finalProduct,
+            mode: "payment",
+            success_url: `${DOMAIN}/success`,
+            cancel_url: `${DOMAIN}/cancel`,
+        });
+        res.json({ sessionUrl: session.url });
+    } catch (error) {
+        console.error("Error creating Stripe Checkout session:", error);
+        res.status(500).json({
+            error: "Failed to create Stripe Checkout session",
+        });
+    }
 });
 
 // Route handler for handling payment success
-router.get('/success', (req, res) => {
-  res.render('success'); // Render success page
+router.get("/success", (req, res) => {
+    res.render("success"); // Render success page
 });
 
 // Route handler for handling payment cancellation
-router.get('/cancel', (req, res) => {
-  res.render('cancel'); // Render cancellation page
+router.get("/cancel", (req, res) => {
+    res.render("cancel"); // Render cancellation page
 });
 
 export default router;
