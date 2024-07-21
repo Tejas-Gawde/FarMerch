d71ac8668e4b63c109d04fe4fd90bdcc2cd73e20
Tejas <tejasg263@gmail.com>
2024-07-21 17:54:25 +0530

    Some Touchups

--- public/javascripts/supabase.js
+++ public/javascripts/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
+import { configDotenv } from 'dotenv';
 
+configDotenv();
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-    "https://xkbaxhajuykyvvzstdfn.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrYmF4aGFqdXlreXZ2enN0ZGZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ3MDEwNTEsImV4cCI6MjAxMDI3NzA1MX0.ymfXSwBn9oK3-M3e597xHgR-m16H7yyAf1mbmJrbEmw");
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY);
